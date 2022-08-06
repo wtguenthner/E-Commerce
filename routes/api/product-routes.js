b0efa2475e014f42391aea1ef8a541c1d07ed96e
Taylor Guenthner <wtguenthner@gmail.com>
2022-08-06 12:15:08 -0400

    Updates to routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -100,8 +100,22 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const userData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!userData) {
+      res.status(404).json({ message: "No user with this id!" });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
